a3a5cb50787e5e2be0717e95e2f6d1792a4b382c
lulu2184 <lulu2184@gmail.com>
2016-06-03 15:54:29 +0800

    constant gap

--- videoStabilization.m
+++ videoStabilization.m
@@ -20,58 +20,68 @@ endm = m - round(m * 0.15);
 startn = round(n * 0.15);
 endn = n - round(n * 0.15);
 n_frame = 0;
-transH = [eye(3)];
-detX, detY, angle = [0];
-img_array = [imgB];
+transH = {eye(3)};
+detX = {0};
+detY = {0};
+angle = {0};
+% img_array = [imgB];
 
 while ~isDone(videoReader)
     disp(n_frame);
     n_frame = n_frame + 1;
-%     imgB = step(converter, frame);
     outA = outB;
     imgA = imgB;
     imgB = step(videoReader);
-%     if mod(n, 50) == 0
-%         Hcumulative = eye(3);
-%         imgA = imgB; 
-%     end;
-%     if i == 3, imshow(imfuse(imgA, imgB,'ColorChannels','red-cyan'));end;
     H = stabilizationTform(rgb2gray(imgA), rgb2gray(imgB));
     HsRt = cvexTformToSRT(H);
-    transH = [transH, HsRt];
 
     Hcumulative = HsRt * Hcumulative;
-    detX = Hcumulative(1,3);
-    detY = Hcumulative(2,3);
-    angle = atan2(Hcumulative(1,1), Hcumulative(2,1));
+    detX{n_frame} = Hcumulative(3,1);
+    detY{n_frame} = Hcumulative(3,2);
+    angle{n_frame} = atan2(Hcumulative(1,1), Hcumulative(2,1));
+    transH{n_frame} = Hcumulative;
 %     Hcumulative = HsRt;
     outB = imwarp(imgB, affine2d(Hcumulative),'OutputView',imref2d(size(imgB)));
         
-    img_array = [img_array, outB];
-    step(videoPlayer, outA);
+%     img_array = [img_array, outB];
+%     step(videoPlayer, outA);
 
-    writeVideo(outVideo, [outB(startm:endm, startn:endn, :)]);
+%     writeVideo(outVideo, [outB(startm:endm, startn:endn, :)]);
 %     step(videoPlayer, imfuse(outA, outB,'ColorChannels','red-cyan'));
 end;
 
-detX = smoothPath(detX, n_frame);
-detY = smoothPath(detY, n_frame);
-angle = smoothPath(angle, n_frame);
+% detX = smoothPath(detX, n_frame);
+% detY = smoothPath(detY, n_frame);
+% angle = smoothPath(angle, n_frame);
 
-gap = 30;
+gap = 50;
 for i = 1:n_frame/gap
-    old = (i-1)*gap;
-    new = i*gap;
-    dx = -(detX(new) - detX(old))/30;
-    dy = -(detY(new) - detY(old))/30;
-    da = -(angle(new) - angle(old))/30;
-    uMat = transH(old)^(-1);
-    for j = 0:gap - 1
-        transH(old + j) = uMat * transH(old + j);
-        uMat = uMat * [cos(da), -sin(da), dx; sin(da), cos(da), dy];
+    old = (i-1)*gap + 1;
+    new = min(i*gap + 1, n_frame);
+    if old == new, break; end;
+    dx = -(detX{new} - detX{old})/(new - old);
+    dy = -(detY{new} - detY{old})/(new - old);
+    da = -(angle{new} - angle{old})/(new - old);
+    uMat = transH{old}^(-1);
+    for j = 0:old - new - 1
+        T = uMat * transH{old + j};
+        T(1, 3) = 0;
+        T(2, 3) = 0;
+        transH{old + j} = T;
+        uMat = uMat * [cos(da), -sin(da), 0; sin(da), cos(da), 0; dx, dy, 1];
     end;
 end;
     
+reset(videoReader);
+for i = 1:n_frame
+    disp(i);
+    img = step(videoReader);
+    out = imwarp(img, affine2d(transH{i}),'OutputView',imref2d(size(img)));
+    out = padarray([out(startm:endm, startn:endn, :)], [startm, startn]);
+    out = out(1:m, 1:n, :);
+    writeVideo(outVideo, [img, out]);
+end;
+        
 close(outVideo);
 release(videoPlayer);
 release(videoReader);
